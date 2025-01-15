a43bce3a6486d02733fdd741c4a8d8c7ec4c0c7d
Imranahmed935 <imrantahir9918@gmail.com>
2025-01-15 10:28:11 +0600

    pms project setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+
+
+  return (
+    <>
+    <div className="text-red-500">
+    <Button>Yes</Button>
+    </div>
+    </>
+  )
+}
+
+export default App
